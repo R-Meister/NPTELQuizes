c08758223ff7d287cd30a5cd60d150a91611780c
R-Meister <raman.66852@gmail.com>
2026-04-17 07:52:25 +0530

    small fix for other courses not accesible

--- src/app/course/page.tsx
+++ src/app/course/page.tsx
@@ -1,10 +1,8 @@
 import CourseSelector from "@/components/CourseSelector";
-import CourseAutoRedirect from "@/components/CourseAutoRedirect";
 
 export default function CoursePage() {
   return (
     <div className="min-h-screen bg-slate-950">
-      <CourseAutoRedirect />
       <CourseSelector />
     </div>
   );
